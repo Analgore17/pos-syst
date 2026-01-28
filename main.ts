e950e9b5cdacbae23b448ff3f76e070edb482c45
analgorenetbugs <anal.g@netbugs.in>
2026-01-28 17:38:25 +0530

    update code

--- main.ts
+++ main.ts
@@ -30,4 +30,4 @@ bootstrapApplication(AppComponent, {
   ]
 }).catch(err => console.error(err));
 
-// AI Studio always uses an `index.tsx` file for all project types.
+// AI Studio always uses an `main.ts` file for all project types.
